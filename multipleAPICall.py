960dfcfa0602c88473d01d80883124499a78e89a
Isabelle LE <32997231+isabelle-le@users.noreply.github.com>
2022-08-29 17:04:29 +0200

    Create multipleAPICall.py

---
+++ multipleAPICall.py
@@ -0,0 +1,62 @@
+import requests as req
+import urllib
+import json
+
+
+def weather_api_call(requestURL, parameters):
+    response = req.get(url=requestURL, params=parameters)
+    # print(response.url)
+    if response.status_code != 200:
+        print(response.json())
+        exit()
+    data = response.json()
+    return json.dumps(data)
+
+
+def get_single_weather( station="paris", key="QRLSXGZEPLWWUC7JLBEQAVQSE", unit_group="metric",
+                       elements="datetime,datetimeEpoch,name,address,resolvedAddress,latitude,longitude,temp,humidity,precip,solarradiation,solarenergy,conditions,icon,stations,source",
+                       include="Cobs", content_type="json"):
+    """
+    :param station
+    :param key: api_key QRLSXGZEPLWWUC7JLBEQAVQSE
+    :param unit_group: metric °c,km
+    :param max_distance(meters): max=200km or 200000m
+    :param elements: values
+    :param include: obs: station observation, remote: remote observation satellite/radar, fcst: forecast,Cobs: combination
+    :param options:useobs,useremote
+    :param content_type:json
+    :return:weather_api_call
+    """
+    requestURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + urllib.parse.quote(station) + "/" + urllib.parse.quote("yesterday")
+    parameters = {"key": key, "unitGroup": unit_group, "elements": elements, "include": include, "contentType": content_type}
+    return weather_api_call(requestURL, parameters)
+
+
+def main():
+    key = "QRLSXGZEPLWWUC7JLBEQAVQSE"
+    get_single_weather_list = get_single_weather( "paris",key, "metric",
+                                                 "datetime,datetimeEpoch,name,address,resolvedAddress,latitude,longitude,temp,humidity,precip,solarradiation,solarenergy,conditions,stations,icon,source",
+                                                 "Cobs", "json")
+    paris_data = json.loads(get_single_weather_list)
+    # print(json.dumps(paris_data))
+    for station in paris_data['days'][0]['stations']:
+        data = json.loads(get_single_weather(station))
+        json_obj = json.dumps(data["days"][0])
+        # python object to be appended
+        json_station = data["stations"][station]
+        # parsing JSON string
+        json_final = json.loads(json_obj)
+        # appending the data
+        json_final.update(json_station)
+        # JSON formatted str
+        json_formatted_str = json.dumps(json_final, indent=10)
+        station_data = json.loads(json_formatted_str)
+        if station_data["solarradiation"] is None:
+            station_data['solarradiation'] = paris_data['days'][0]['solarradiation']
+        if station_data["solarenergy"] is None:
+            station_data['solarenergy'] = paris_data['days'][0]['solarenergy']
+        json_formatted_str = json.dumps(station_data, indent=10)
+        print(json_formatted_str)
+
+if __name__ == "__main__":
+    main()
